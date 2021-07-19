bc501b9d6783d538da48a7db92dd7b8709ee498a
tamirer <tamirer@post.bgu.ac.il>
2021-07-19 13:19:29 +0300

    Integration with PostgreSQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,11 @@
 DROP TABLE IF EXISTS squads;
 
-CREATE TABLE squads (
-                              id Long AUTO_INCREMENT PRIMARY KEY,
-                              name VARCHAR(250) NOT NULL
+CREATE TABLE squads
+(
+    id   LONG PRIMARY KEY,
+    name VARCHAR(250) NOT NULL
 );
 
-INSERT INTO squads (name) VALUES ('069');
-INSERT INTO squads (name) VALUES ('100');
-INSERT INTO squads (name) VALUES ('114');
+INSERT INTO squads (id,name) VALUES (1,'069');
+INSERT INTO squads (id,name) VALUES (2,'100');
+INSERT INTO squads (id,name) VALUES (3,'114');
