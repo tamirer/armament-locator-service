1280b813b01443eaf2dd9f0a94f75fefd811ff31
pazrein <pazreingold1996@gmail.com>
2021-07-19 09:47:54 +0300

    First commit

---
+++ src/main/java/iaf/blueflame/armamentlocatorservice/ArmamentLocatorServiceApplication.java
@@ -0,0 +1,13 @@
+package iaf.blueflame.armamentlocatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmamentLocatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmamentLocatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iaf/blueflame/armamentlocatorservice/ArmamentLocatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package iaf.blueflame.armamentlocatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmamentLocatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
