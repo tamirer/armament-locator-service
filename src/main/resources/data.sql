134e324e6c9fed44726691d0c26f0a3cfa92f445
pazrein <pazreingold1996@gmail.com>
2021-07-19 15:47:15 +0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,43 @@
+DROP TABLE IF EXISTS tails;
 DROP TABLE IF EXISTS squads;
+DROP TABLE IF EXISTS mission_statuses;
 
 CREATE TABLE squads
 (
-    id   LONG PRIMARY KEY,
-    name VARCHAR(250) NOT NULL
+    id          integer PRIMARY KEY,
+    description VARCHAR(250) NOT NULL
+);
+
+INSERT INTO squads (id, description)
+VALUES (1, '069');
+INSERT INTO squads (id, description)
+VALUES (2, '100');
+INSERT INTO squads (id, description)
+VALUES (3, '114');
+
+CREATE TABLE tails
+(
+    id             integer PRIMARY KEY,
+    description    VARCHAR(250) NOT NULL,
+    squad          integer      not null,
+    mission_status integer      not null
+);
+
+insert into tails (id, description, squad, mission_status)
+values (1, '212', 1, 1);
+insert into tails (id, description, squad, mission_status)
+values (2, '987', 1, 2);
+insert into tails (id, description, squad, mission_status)
+values (3, '404', 2, 3);
+insert into tails (id, description, squad, mission_status)
+values ( 4, '210'
+       , 1, 4);
+
+
+CREATE TABLE mission_statuses
+(
+    id          integer PRIMARY KEY,
+    description VARCHAR(250) NOT NULL
 );
 
 INSERT INTO mission_statuses (id, description)
@@ -17,6 +51,9 @@ VALUES (4, 'AfterFlightArmaments');
 INSERT INTO mission_statuses (id, description)
 VALUES (5, 'Done');
 
-INSERT INTO squads (id,name) VALUES (1,'069');
-INSERT INTO squads (id,name) VALUES (2,'100');
-INSERT INTO squads (id,name) VALUES (3,'114');
+select *
+from tails;
+select *
+from squads;
+select *
+from mission_statuses;
